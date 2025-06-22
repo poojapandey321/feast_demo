8f2386c90236f23a5b7afd7478a651fe09a10db4
pooja pandey <poojapandey@MacBookAir.lan>
2025-06-22 15:58:25 +0530

    i

--- iris_data/iris_data.py
+++ iris_data/iris_data.py
@@ -2,14 +2,23 @@ import pandas as pd
 from datetime import timedelta
 from feast.types import Float32
 from feast import Entity, FeatureView, Field, FileSource, ValueType
+import os
 
-# STEP 1: Convert iris.csv to iris_feast.csv (with flower_id + event_timestamp)
 def prepare_iris_csv():
-    df = pd.read_csv("../data/iris.csv")
+    # Resolve base directory where the script is located
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # Construct full paths
+    iris_input_path = os.path.join(base_dir, "../data/iris.csv")
+    iris_output_path = os.path.join(base_dir, "../data/iris_feast.csv")
+
+    # Read and write
+    df = pd.read_csv(iris_input_path)
     df.insert(0, "flower_id", range(1, len(df) + 1))
     df["event_timestamp"] = pd.to_datetime("2025-06-01T10:00:00")
-    df.to_csv("../data/iris_feast.csv", index=False)
-    print("iris_feast.csv created.")
+    df.to_csv(iris_output_path, index=False)
+
+    print(f"iris_feast.csv created at {iris_output_path}")
 
 prepare_iris_csv()
 
